fc87daa69bc0d6b3e9dba89019cc97135a2eafb5
Jose Benaya Rehiary <122864263+jsrehiary@users.noreply.github.com>
2026-05-05 08:50:17 +0700

    Update BulletBehaviour.cs

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -4,7 +4,7 @@ public class BulletBehaviour : MonoBehaviour
 {
     [Header("Movement")]
     public float speed = 20f;
-    public Vector3 direction = Vector3.forward;
+    public Vector3 direction = Vector3.back;
 
     [Header("Combat")]
     public int damage = 1;
